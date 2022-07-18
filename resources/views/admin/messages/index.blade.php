0f64ef595a35337a4e2f896876cae69ea622245f
PepperFM <Damon3453@yandex.ru>
2022-07-18 19:46:26 +0500

    [fix] formatting

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,33 +1,37 @@
 @extends('dashboard')
 @section('title')
-Messages
+    Messages
 @endsection
 
 @section('content')
 
-<td><a href="{{ route('admin.create') }}" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Create new message</a></td>
+    <td><a href="{{ route('admin.create') }}"
+           class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Create
+            new message</a></td>
 
-<table class="border-collapse table-auto w-full text-sm">
-    <thead>
-      <tr>
-        <th>user</th>
-        <th>content</th>
-        <th>time</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach($messages as $message)
+    <table class="border-collapse table-auto w-full text-sm">
+        <thead>
         <tr>
-            <td>{{ $message->user->name }}</td>
-            <td>{{ $message->content }}</td>
-            <td>{{ $message->created_at->diffForHumans() }}</td>
-            @if($message->is_new && $message->user_id == auth()->user()->id)
-            <td><a href="{{ route('admin.destroy', $message) }}" class="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">Удалить</a></td>
-            @endif
+            <th>user</th>
+            <th>content</th>
+            <th>time</th>
         </tr>
+        </thead>
+        <tbody>
+        @foreach($messages as $message)
+            <tr>
+                <td>{{ $message->user->name }}</td>
+                <td>{{ $message->content }}</td>
+                <td>{{ $message->created_at->diffForHumans() }}</td>
+                @if($message->is_new && $message->user_id == auth()->user()->id)
+                    <td><a href="{{ route('admin.destroy', $message) }}"
+                           class="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">Удалить</a>
+                    </td>
+                @endif
+            </tr>
         @endforeach
-    </tbody>
-</table>
+        </tbody>
+    </table>
 
-{{ $messages->links() }}
+    {{ $messages->links() }}
 @stop
