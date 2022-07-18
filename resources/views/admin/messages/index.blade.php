885c7e83a25f4f6b4a142d3e20de023d8a6cd9b4
PepperFM <Damon3453@yandex.ru>
2022-07-18 19:37:45 +0500

    [add] TW button styles

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -5,26 +5,28 @@ Messages
 
 @section('content')
 
-<td><a href="{{ route('admin.create') }}" class="btn btn-primary btn-sm">Create new message</a></td>
+<td><a href="{{ route('admin.create') }}" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Create new message</a></td>
 
-<table class="table" >
+<table class="border-collapse table-auto w-full text-sm">
     <thead>
       <tr>
-        <th scope="col">user</th>
-        <th scope="col">content</th>
-        <th scope="col">time</th>
+        <th>user</th>
+        <th>content</th>
+        <th>time</th>
       </tr>
     </thead>
+    <tbody>
         @foreach($messages as $message)
         <tr>
             <td>{{ $message->user->name }}</td>
             <td>{{ $message->content }}</td>
             <td>{{ $message->created_at->diffForHumans() }}</td>
             @if($message->is_new && $message->user_id == auth()->user()->id)
-            <td><a href="{{ route('admin.destroy', $message) }}" class="btn btn-outline-warning">Удалить</a></td>
+            <td><a href="{{ route('admin.destroy', $message) }}" class="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">Удалить</a></td>
             @endif
         </tr>
         @endforeach
+    </tbody>
 </table>
 
 {{ $messages->links() }}
